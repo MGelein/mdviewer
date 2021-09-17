82344ff593b0e0bd37923aca2f7547bdf3489b37
Mees Gelein <meesgelein@gmail.com>
2021-09-18 00:26:08 +0200

    feat: navbar for the editor window is mostly done

--- src/components/MainView/MainView.tsx
+++ src/components/MainView/MainView.tsx
@@ -1,9 +1,12 @@
 import React from "react"
+import OpenNav from "../OpenNav";
 
 import './main-view.scss';
 
 const MainView: React.FC = () => {
-    return <div className="main-view"></div>
+    return (<div className="main-view">
+        <OpenNav />
+    </div>);
 }
 
 export default MainView;
---
+++ src/components/OpenTab/OpenTab.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useApp } from "../../util/hooks";
+import Button from "../Button";
+import Icon from "../Icon";
+
+import './open-tab.scss';
+
+type Props = {
+    url: string;
+}
+
+const OpenTab: React.FC<Props> = ({ url }) => {
+    const { focusFile, setFocusFile, setOpenFiles, openFiles } = useApp();
+    const isFocusFile = url === focusFile;
+
+    const makeFocus = () => {
+        if (!isFocusFile) setFocusFile(url);
+    }
+
+    const closeFile = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        setOpenFiles(files => {
+            const currentIndex = files.indexOf(url);
+            if (isFocusFile) {
+                const newFocusFile = files[currentIndex + 1 < files.length ? currentIndex + 1 : currentIndex - 1];
+                setFocusFile(newFocusFile);
+            }
+
+            const newFiles = files.filter((openFile) => {
+                return openFile !== url;
+            });
+            return newFiles;
+        });
+    }
+
+    return (<div onClick={isFocusFile ? undefined : makeFocus} className={`open-tab ${isFocusFile ? 'focus' : ''}`}>
+        {url}
+        <div className="open-tab__close" onClick={closeFile}><Icon name="close" /></div>
+    </div>);
+}
+
+export default OpenTab;
