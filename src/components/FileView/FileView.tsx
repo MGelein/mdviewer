7a941f015e5ef5b609612306d862a2ced5349868
Mees Gelein <meesgelein@gmail.com>
2021-09-22 22:50:41 +0200

    feat: change edit mode for focusFile using viewControls

--- src/components/FileView/FileView.tsx
+++ src/components/FileView/FileView.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useApp } from "../../util/hooks";
+import { EditMode } from "../../util/types";
 import MDPreview from "../MDPreview";
 import ViewControls from "../ViewControls";
 import ViewPlaceholder from "../ViewPlaceholder";
@@ -7,14 +8,16 @@ import ViewPlaceholder from "../ViewPlaceholder";
 import './file-view.scss';
 
 const FileView: React.FC = () => {
-    const { openFiles, fileData } = useApp();
+    const { openFiles, fileData, editModes, focusFile } = useApp();
+    const editMode: EditMode = focusFile ? editModes[focusFile] : 'preview';
     const anyOpenFileClass = openFiles.length > 0 ? 'any-open' : '';
 
     return (<div className={`file-view ${anyOpenFileClass}`}>
         {fileData.length > 1 ?
             <>
                 <ViewControls />
-                <MDPreview />
+                {editMode === 'preview' && <MDPreview />}
+                {editMode === 'edit' && <></>}
             </>
             :
             <ViewPlaceholder />
--- src/components/ViewControls/ViewControls.tsx
+++ src/components/ViewControls/ViewControls.tsx
@@ -1,14 +1,43 @@
 import React from "react";
+import { useApp } from "../../util/hooks";
+import { EditMode } from "../../util/types";
 import Icon from "../Icon";
 
 import './view-controls.scss';
 
 const ViewControls: React.FC = () => {
+    const { editModes, setEditModes, focusFile } = useApp();
+    const editMode: EditMode = focusFile ? editModes[focusFile] : 'preview';
+
+    const changeMode = (mode: EditMode) => {
+        if (!focusFile) return;
+        const newModes = { ...editModes };
+        newModes[focusFile] = mode;
+        setEditModes(newModes);
+    }
+    const openEdit = () => changeMode('edit');
+    const openPreview = () => changeMode('preview');
+
     return <div className="view-controls">
-        <span title="Edit" className="view-controls__control"><Icon name="edit" /></span>
-        <span title="Preview" className="view-controls__control"><Icon name="visibility" /></span>
-        <span title="Save" className="view-controls__control"><Icon name="save" /></span>
-        <span title="Remove" className="view-controls__control"><Icon name="delete" /></span>
+        {editMode === 'edit' ?
+            <>
+                <span onClick={openPreview} title="Preview" className="view-controls__control">
+                    <Icon name="visibility" />
+                </span>
+                <span title="Save" className="view-controls__control">
+                    <Icon name="save" />
+                </span>
+            </>
+            :
+            <>
+                <span onClick={openEdit} title="Edit" className="view-controls__control">
+                    <Icon name="edit" />
+                </span>
+                <span title="Remove" className="view-controls__control">
+                    <Icon name="delete" />
+                </span>
+            </>
+        }
     </div>
 }
 
