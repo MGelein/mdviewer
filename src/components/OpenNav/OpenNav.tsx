7081bde800d27c0fc0e0213b6b35f70d3979ba5e
Mees Gelein <meesgelein@gmail.com>
2022-01-22 00:44:00 +0100

    feat: added the final two hotkeys for now

--- src/components/OpenNav/OpenNav.tsx
+++ src/components/OpenNav/OpenNav.tsx
@@ -1,11 +1,24 @@
-import React from "react";
-import { useApp } from "../../util/hooks";
+import React, { useCallback } from "react";
+import { saveMarkdown } from "../../util/file";
+import { useApp, useHotkey } from "../../util/hooks";
 import OpenTab from "../OpenTab";
 
 import './open-nav.scss';
 
 const OpenNav: React.FC = () => {
-    const { openFiles } = useApp();
+    const { openFiles, focusFile, workdir, setFocusFile } = useApp();
+
+    const changeTab = useCallback((direction: 1 | -1) => {
+        if (!focusFile || openFiles.length <= 1) return;
+        const tabIndex = openFiles.indexOf(focusFile);
+        saveMarkdown(focusFile, workdir);
+        const nextIndex = (tabIndex + direction) + openFiles.length;
+        const nextFile = openFiles[nextIndex % openFiles.length];
+        setFocusFile(nextFile);
+    }, [focusFile, openFiles, workdir, setFocusFile]);
+
+    useHotkey('ctrl+tab', () => changeTab(1));
+    useHotkey('ctrl+shift+tab', () => changeTab(-1));
 
     return (<nav className="open-nav">
         {openFiles.map((openFile, index) => {
